95dc3ded0337f89fe3dbe78c65b56d00edebc0b1
GitMasterNikanjam <nikanjam.farazcomm@gmail.com>
2024-01-07 14:34:24 +0330

    add TinyGPS++ library

---
+++ Libraries/TinyGPSPlus/src/test.cpp
@@ -0,0 +1,13 @@
+// for compile and build: g++ -o test test.cpp 
+
+#include <iostream>
+#include "TinyGPS++.h"
+
+
+using namespace std;
+
+int main(void)
+{    
+    cout<< _GPS_VERSION<< endl;
+    return 0;
+}
